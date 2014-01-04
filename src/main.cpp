e6c21010635c91a70535e089554d38ae520102aa
Marco <marco@debian-i7.(none)>
2014-01-04 19:24:00 +0100

    reduce nLimit in order to reduce processing time when receiving a getblocks request

--- src/main.cpp
+++ src/main.cpp
@@ -3635,7 +3635,10 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         // Send the rest of the chain
         if (pindex)
             pindex = pindex->pnext;
-        int nLimit = 500;
+
+        // reduce nLimit in order to reduce processing time when receiving a getblocks request
+        int nLimit = 50; //500;
+
         printf("getblocks %d to %s limit %d\n", (pindex ? pindex->nHeight : -1), hashStop.ToString().c_str(), nLimit);
         for (; pindex; pindex = pindex->pnext)
         {
